85062ee1d275dbe3721abc92752f702e9c63fd4e
Alan Gabbianelli <alan.gabbianelli@gmail.com>
2016-01-26 10:23:57 +0000

    Adds GitHub API

---
+++ test/gitUserSearchController.spec.js
@@ -0,0 +1,46 @@
+describe('GitUserSearch Controller', function() {
+  beforeEach(module('GitUserSearch'));
+
+  var ctrl;
+
+  beforeEach(inject(function($controller) {
+    ctrl = $controller('GitUserSearchController');
+  }));
+
+  it('initializes with an empty search result and term', function() {
+    expect(ctrl.searchResult).toBeUndefined();
+    expect(ctrl.searchTerm).toBeUndefined();
+  });
+
+  describe('when searching for a user', function() {
+    var httpBackend;
+    beforeEach(inject(function($httpBackend) {
+      httpBackend = $httpBackend;
+      httpBackend
+      .when('GET', 'https://api.github.com/search/users?q=tansaku')
+      .respond(
+        { items: items }
+      );
+    }));
+
+    var items = [
+      {
+        'login': 'tansaku',
+        'avatar_url': 'https://avatars.githubusercontent.com/u/30216?v=3',
+        'html_url': 'https://github.com/tansaku'
+      },
+      {
+        'login': 'stephenlloyd',
+        'avatar_url': 'https://avatars.githubusercontent.com/u/196474?v=3',
+        'html_url': 'https://github.com/stephenlloyd'
+      }
+    ];
+
+    it('displays search results', function() {
+      ctrl.searchTerm = 'tansaku';
+      ctrl.doSearch();
+      httpBackend.flush();
+      expect(ctrl.searchResult.items).toEqual(items);
+    });
+  });
+});
